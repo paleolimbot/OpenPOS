5992a3c9a05d002459870ba349caa853a80da041
Dewey Dunnington <dewey@fishandwhistle.net>
2016-10-03 19:12:45 -0400

    move text API cache to database

--- app/src/main/java/net/fishandwhistle/openpos/api/TextApiCache.java
+++ app/src/main/java/net/fishandwhistle/openpos/api/TextApiCache.java
@@ -1,6 +1,10 @@
 package net.fishandwhistle.openpos.api;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
@@ -20,74 +24,83 @@ import java.security.NoSuchAlgorithmException;
 public class TextApiCache {
 
     private static final String TAG = "TextApiCache";
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "net.fishandwhistle.TextApiCache";
 
-    private Context context ;
+    private CacheDB db;
 
     public TextApiCache(Context context) {
-        this.context = context;
+        this.db = new CacheDB(context);
     }
 
-    private File getFile(String url) {
-        return new File(context.getCacheDir(), digest(url));
-    }
-
-    private boolean contains(String url) {
-        return getFile(url).exists();
+    public boolean contains(String url) {
+        return this.get(url) != null;
     }
 
     public String get(String url) {
-        File f = getFile(url);
-        if(f.exists()) {
-            // read file
-            try {
-                FileInputStream fis = new FileInputStream(f);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                byte[] buffer = new byte[256];
-                int count = 0;
-                while ((count = fis.read(buffer)) != -1) {
-                    baos.write(buffer, 0, count);
-                }
-                fis.close();
-                String out = baos.toString("UTF-8");
-                baos.close();
-                return out;
-            } catch(IOException e) {
-                Log.e(TAG, "get: IO exception", e);
-                return null;
-            }
-        } else {
-            return null;
-        }
+        return db.getEntry(url);
     }
 
     public boolean put(String url, String data) {
-        File f = getFile(url);
-
-        // write file
-        try {
-            FileOutputStream fos = new FileOutputStream(f);
-            fos.write(data.getBytes("UTF-8"));
-            fos.close();
-            return true;
-        } catch(IOException e) {
-            Log.e(TAG, "get: IO exception", e);
-            return false;
-        }
+        return db.putEntry(url, data);
     }
 
 
-    private static String digest(String in) {
-        try {
-            MessageDigest d = MessageDigest.getInstance("MD5");
-            byte[] b = d.digest(in.getBytes("UTF-8"));
-            BigInteger bigInt = new BigInteger(1, b);
-            return bigInt.toString(16);
-        } catch (NoSuchAlgorithmException e) {
-            Log.e(TAG, "digest: no md5 digest algorithm");
-            throw new RuntimeException(e.getMessage());
-        } catch (UnsupportedEncodingException e) {
-            Log.e(TAG, "digest: no utf8 encoding");
-            throw new RuntimeException(e.getMessage());
+    private static class CacheDB extends SQLiteOpenHelper {
+
+        public CacheDB(Context context) {
+            super(context, DB_NAME, null, DB_VERSION);
+        }
+
+        public boolean putEntry(String url, String response) {
+            SQLiteDatabase db = this.getWritableDatabase();
+            boolean success ;
+            if(response != null) {
+                //insert/update
+                ContentValues cv = new ContentValues();
+                cv.put("theurl", url);
+                cv.put("response", response);
+                Cursor c = db.query("cached_responses", null, "theurl=?", new String[]{url}, null, null, null, "1");
+                if (c.getCount() == 0) {
+                    long id = db.insert("cached_responses", null, cv);
+                    success = id != -1;
+                } else {
+                    int nrows = db.update("cached_responses", cv, "theurl=?", new String[]{url});
+                    success = nrows > 0;
+                }
+                c.close();
+            } else {
+                //remove
+                int rows = db.delete("cached_responses", "theurl=?", new String[] {url});
+                success = rows > 0;
+            }
+            db.close();
+            return success;
+        }
+
+        public String getEntry(String url) {
+            SQLiteDatabase db = this.getReadableDatabase();
+            Cursor c = db.query("cached_responses", null, "theurl=?", new String[] {url}, null, null, null, "1");
+            String out = null;
+            if(c.getCount() > 0) {
+                c.moveToFirst();
+                out = c.getString(1);
+            }
+            c.close();
+            return out;
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE cached_responses (theurl TEXT PRIMARY KEY, "
+                    + "response TEXT, "
+                    + "extra TEXT)");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            //only one db version
         }
     }
+
 }
