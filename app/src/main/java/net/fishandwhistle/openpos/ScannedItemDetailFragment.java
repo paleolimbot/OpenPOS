f1df6860ca032d5dfbda15fc428a99277dc7d039
Dewey Dunnington <dewey@fishandwhistle.net>
2016-10-16 14:34:50 -0400

    update details dialog

--- app/src/main/java/net/fishandwhistle/openpos/ScannedItemDetailFragment.java
+++ app/src/main/java/net/fishandwhistle/openpos/ScannedItemDetailFragment.java
@@ -1,29 +1,40 @@
 package net.fishandwhistle.openpos;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import net.fishandwhistle.openpos.items.ScannedItem;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Locale;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ScannedItemDetailFragment extends DialogFragment {
-
+    private static final String TAG = "ScannedItemDetailFragment";
     public static final String ARG_ITEM = "arg_item";
     private ScannedItem item;
 
@@ -33,6 +44,22 @@ public class ScannedItemDetailFragment extends DialogFragment {
     @Override
     public void onResume() {
         super.onResume();
+
+
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.title_activity_scanned_item_detail);
+        builder.setPositiveButton(R.string.detail_close, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "onClick: close!");
+                    }
+                });
+        View v = getActivity().getLayoutInflater().inflate(R.layout.fragment_scanned_item_detail, null);
+
         Serializable s ;
         if(getArguments() != null && getArguments().containsKey(ARG_ITEM)) {
             s = getArguments().getSerializable(ARG_ITEM);
@@ -43,30 +70,20 @@ public class ScannedItemDetailFragment extends DialogFragment {
         }
 
         if(s != null) {
-            setItem((ScannedItem) s, getView());
+            setItem((ScannedItem) s, v);
         } else {
-            setItem(null, getView());
+            setItem(null, v);
         }
+        builder.setView(v);
 
-        if(getDialog() != null) {
-            getDialog().setTitle(R.string.title_activity_scanned_item_detail);
-            DisplayMetrics metrics = getResources().getDisplayMetrics();
-            int width = metrics.widthPixels;
-            int height = metrics.heightPixels;
-            if(getDialog().getWindow() != null) {
-                getDialog().getWindow().setLayout((6 * width) / 7, (4 * height) / 5);
-            }
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scanned_item_detail, container, false);
+        return builder.create();
     }
 
     private void setItem(ScannedItem item, View v) {
         this.item = item;
+        TableLayout kv = (TableLayout)v.findViewById(R.id.detail_keyvalues);
+        kv.removeAllViews();
+
         if(item != null) {
             if(item.description != null) {
                 ((TextView) v.findViewById(R.id.detail_description)).setText(item.description);
@@ -74,22 +91,32 @@ public class ScannedItemDetailFragment extends DialogFragment {
                 ((TextView) v.findViewById(R.id.detail_description)).setText(String.format(getString(R.string.detail_nodescription), item.barcodeType));
             }
             ((TextView)v.findViewById(R.id.detail_quantity)).setText(String.format(getString(R.string.detail_qty), item.nScans));
-            ((TextView)v.findViewById(R.id.detail_scantime)).setText(String.format(getString(R.string.detail_scantime),
-                    formatTime(item.updateTime)));
-            ((TextView)v.findViewById(R.id.detail_codetype)).setText(String.format(getString(R.string.detail_codetype), item.barcodeType));
-            ((TextView)v.findViewById(R.id.detail_code)).setText(item.productCode);
-            TextView json = (TextView)v.findViewById(R.id.detail_json);
-            TextView jsonSource = (TextView)v.findViewById(R.id.detail_jsonsource);
+
+            ArrayList<String[]> vals = new ArrayList<>();
+            vals.add(new String[] {getString(R.string.detail_codetype), item.barcodeType});
+            vals.add(new String[] {getString(R.string.detail_scantime), formatTime(item.updateTime)});
+
             if(item.json != null) {
-                json.setVisibility(View.VISIBLE);
-                jsonSource.setVisibility(View.VISIBLE);
-                json.setText(formatText(item.json));
-                jsonSource.setText(String.format(getString(R.string.details_jsonsource),
-                        formatText(item.jsonSource), formatTime(item.jsonTime)));
-            } else {
-                json.setVisibility(View.GONE);
-                jsonSource.setVisibility(View.GONE);
+                try {
+                    JSONObject o = new JSONObject(item.json);
+                    Iterator<String> keys = o.keys();
+                    while(keys.hasNext()) {
+                        String key = keys.next();
+                        vals.add(new String[] {key, o.getString(key)});
+                    }
+                } catch(JSONException e) {
+                    vals.add(new String[] {"json_error", e.getMessage()});
+                }
             }
+
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            for(String[] keyval: vals) {
+                View row = inflater.inflate(R.layout.scanned_item_details_kv, null);
+                ((TextView)row.findViewById(R.id.key)).setText(keyval[0]);
+                ((TextView)row.findViewById(R.id.value)).setText(keyval[1]);
+                kv.addView(row);
+            }
+
         } else {
             ((TextView)v.findViewById(R.id.detail_description)).setText(formatText("no item to show"));
         }
